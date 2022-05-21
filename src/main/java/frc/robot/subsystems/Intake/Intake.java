2d9fb095c90c20d9f91ac5ef99a653b155565abc
ac078 <104099649+ac078@users.noreply.github.com>
2022-05-21 10:35:15 -0400

    Update Intake.java

--- src/main/java/frc/robot/subsystems/Intake/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake/Intake.java
@@ -1,1 +1,9 @@
+package frc.robot.subsystems.intake;
+
+import com.kauailabs.navx.frc.AHRS;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel;
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.CANSparkMax.IdleMode;
+
 
