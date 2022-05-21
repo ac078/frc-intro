92661bc442c6c474f04e7dc6ee32af410343123b
ac078 <104099649+ac078@users.noreply.github.com>
2022-05-21 10:58:41 -0400

    Update Intake.java

--- src/main/java/frc/robot/subsystems/Intake/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake/Intake.java
@@ -5,14 +5,46 @@ import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel;
 import com.revrobotics.RelativeEncoder;
 import com.revrobotics.CANSparkMax.IdleMode;
-
-import edu.wpi.first.math.filter.SlewRateLimiter;
-import edu.wpi.first.math.kinematics.DifferentialDriveOdometry;
-import edu.wpi.first.wpilibj.SPI;
-import edu.wpi.first.wpilibj.drive.DifferentialDrive;
-import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
 public class Intake extends SubsystemBase {
+   
+  private CANSparkMax Intake;
+  private RelativeEncoder encoder;
+  private SparkMaxPIDController controller;
+
+  public Intake(){
+  
+        this.intake = new CANSparkMax(Intake.kPort, CANSparkMax.IntakeType.kBrushless);
+        this.encoder = this.intake.getEncoder();
+
+        this.controller = this.intake.getPIDController();
+        this.controller.setP(Intake.kP); this.controller.setI(Intake.kI); this.controller.setD(Intake.kD); this.controller.setFF(Intake.kFF);
+    }
+
+ public void setPower(double power){
+        this.intake.set(power);
+    }
+
+    public double getPosition(){
+        return this.encoder.getPosition();
+    }
+    public void setPosition(double position){
+        this.controller.setReference(position, CANSparkMax.ControlType.kPosition);
+    }
+    public void setEncoder(double count){
+        this.encoder.setPosition(count);
+    }
+
+    public double getVelocity(){
+        return this.encoder.getVelocity();
+    }
+    public void setVelocity(double velocity){
+        this.controller.setReference(velocity, CANSparkMax.ControlType.kVelocity);
+    }
+    public void stop() {
+    this.intake.set(0);
+    }
+}
   
