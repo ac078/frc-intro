7e7621617669af881a77b26b45094c8cacf99816
ac078 <104099649+ac078@users.noreply.github.com>
2022-05-21 10:39:18 -0400

    Update Constants.java

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -1,6 +1,12 @@
 package frc.robot;
 
 public class Constants {
+     private final static int kPort = 0;
+    private final static double kP = 0;
+    private final static double kI = 0;
+    private final static double kD = 0;
+    private final static double kFF = 0;
+    
     public static final class Trajectory {
         public static final double kDistPerRot = 3.072 / 100;
     }
